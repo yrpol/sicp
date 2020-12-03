2f55a527509aac0f1c3eab3041ba84d130dce000
Yurii Polishchuk <yr.polishchuk@gmail.com>
2020-12-03 17:06:38 +0200

    test with 03.rkt

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -1,21 +1,6 @@
-#lang sicp
+#lang planet neil/sicp
 
-(#%require rackunit)
-
-(define solution
-  (/ (+ 5
-        4
-        (- 2
-           (- 3
-              (+ 6
-                 (/ 4
-                    (dec
-                     (inc
-                      5)))))))
-     (* 3
-        (- 6
-           2)
-        (- 2
-           7))))
-
-(check-equal? solution -37/150)
+(define (function x y z)
+  (cond ((and (> x y) (> z y)) (+ (* x x) (* z z)))
+        ((and (< x y) (< x z)) (+ (* y y) (* z z)))
+        ((and (> x z) (> y z)) (+ (* x x) (* y y)))))
