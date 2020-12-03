fe1d21a79201f2847cb516ceb5564dab088d9d69
yrpol <yrpol87@gmail.com>
2020-12-03 17:15:07 +0200

    Update 03.rkt

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -1,6 +1,10 @@
-#lang planet neil/sicp
+#lang sicp
+
+(#%require rackunit)
 
 (define (function x y z)
   (cond ((and (> x y) (> z y)) (+ (* x x) (* z z)))
         ((and (< x y) (< x z)) (+ (* y y) (* z z)))
         ((and (> x z) (> y z)) (+ (* x x) (* y y)))))
+
+(check-equal? (function 2 4 6) 52)
